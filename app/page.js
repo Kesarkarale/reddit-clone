18fc9d3b85102923d3767a7dbb71412156766b29
Kesar Karale <kesarkarale1@gmail.com>
2026-05-15 00:35:03 +0530

    Add ThemeToggle component import to page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar";
 import FloatingActions from "../components/FloatingActions";
 import Footer from "../components/Footer";
+import ThemeToggle from "../components/ThemeToggle";
 
 export default function Home() {
   return (
